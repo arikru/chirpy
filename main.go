64ef433487933881846e0ec81a90b152d9bd2a44
arikru <krumbach.arian@gmail.com>
2024-07-31 17:18:28 +0200

    feat: handlers added

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	const filePath = "."
+	const port = "8080"
+
 	mux := http.NewServeMux()
-	server := http.Serve
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePath))))
+	mux.Handle("/app/assets/logo.png", http.FileServer(http.Dir(filePath+"/assets/logo.png")))
+
+	healthzHandler := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.Write([]byte("OK"))
+	}
+
+	mux.HandleFunc("/healthz", healthzHandler)
+	server := http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	fmt.Printf("Serving files from %s on port: %v", filePath, port)
+	log.Fatal(server.ListenAndServe())
 }
